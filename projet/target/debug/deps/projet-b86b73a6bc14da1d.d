a8382d5fc24ed17c848c9e225cff0d7949a594f6
Rafael FERNANDO <rafaelfernando@MacBook-Pro-de-Rudy-2.local>
2025-12-04 22:06:36 +0100

    init

---
+++ projet/target/debug/deps/projet-b86b73a6bc14da1d.d
@@ -0,0 +1,5 @@
+/Users/rafaelfernando/Desktop/cours/rust/projet/target/debug/deps/projet-b86b73a6bc14da1d.d: src/main.rs
+
+/Users/rafaelfernando/Desktop/cours/rust/projet/target/debug/deps/projet-b86b73a6bc14da1d: src/main.rs
+
+src/main.rs:
---
+++ projet/target/debug/projet.d
@@ -0,0 +1,1 @@
+/Users/rafaelfernando/Desktop/cours/rust/projet/target/debug/projet: /Users/rafaelfernando/Desktop/cours/rust/projet/src/main.rs
