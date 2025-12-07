74e0df1000ed7665a63204600e538e764bcdac23
Rafael FERNANDO <rafaelfernando@MacBook-Pro-de-Rudy-2.local>
2025-12-07 17:18:25 +0100

    structure FAT32 v1

--- projet/src/lib.rs
+++ projet/src/lib.rs
@@ -6,3 +6,10 @@ use alloc::vec::Vec;
 pub trait BlockDevice {
 fn read_sector(&mut self, lba: u64, sector: &mut [u8; 512]);
 }
+
+pub struct Fat32<D: BlockDevice> {
+dev: D,
+bytes_per_sector: u16,
+sectors_per_cluster: u8,
+root_cluster: u32,
+}
