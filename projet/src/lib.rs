9a61873b66c4a02c05107ba41b671e54c4e295c3
Rafael FERNANDO <rafaelfernando@MacBook-Pro-de-Rudy-2.local>
2025-12-12 14:35:54 +0100

    ajout structure bpb v1

--- projet/src/lib.rs
+++ projet/src/lib.rs
@@ -8,14 +8,24 @@ fn read_sector(&mut self, lba: u64, sector: &mut [u8; 512]);
 }
 
 pub struct Fat32<D: BlockDevice> {
-dev: D,
-bytes_per_sector: u16,
-sectors_per_cluster: u8,
-root_cluster: u32,
-reserved_sectors: u16, 
-number_of_fats: u8,
-sectors_per_fat: u32
+    dev: D,
+    bytes_per_sector: u16,
+    sectors_per_cluster: u8,
+    root_cluster: u32,
+    reserved_sectors: u16, 
+    number_of_fats: u8,
+    sectors_per_fat: u32
 }
+
+ struct Bpb { 
+    bytes_per_sector: u16,
+    sectors_per_cluster: u8,
+    reserved_sectors: u16,
+    number_of_fats: u8,
+    sectors_per_fat: u32,
+    root_cluster: u32,
+}
+
 
 impl<D: BlockDevice> Fat32<D> { // Read du boot sector
     pub fn new(mut dev: D) -> Self {
